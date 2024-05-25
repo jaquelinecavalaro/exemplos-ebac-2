ebe5f57cf816bcbc37b15e304b8332d2233cd020
Jaqueline C <jaqueline.cavalaro@gmail.com>
2024-05-25 12:44:34 -0300

    feat: add new exercice wrapper

---
+++ tarefa_wrapper/ExercicioWrapper.java
@@ -0,0 +1,20 @@
+package tarefa_wrapper;
+
+import java.util.Scanner;
+
+public class ExercicioWrapper{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);    
+    /*
+     *criar uma classe e dentro dela ler do console um valor numerico, 
+     coloque numa variavel primitiva, faça conversão pro tipo wrapper dessa variavel   
+     */
+        System.out.println("Informe um numero inteiro: ");
+        int numero = scanner.nextInt();
+        Integer numInteger = numero;
+
+        System.out.println("Esse numero em Integer é: "+ numInteger);
+    }
+}
